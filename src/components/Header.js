3c0711757123f3755889b23a5a54f46c301f7866
nazishshaheen <nazishrapp@gmail.com>
2024-05-19 23:12:57 +0530

    added heading component

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import '../style.css';
+
+function Header() {
+    return (
+        <div className="Justify-content-center Header">
+            <h1>Typist Pro</h1>
+        </div>
+    )
+}
+
+export default Header;
