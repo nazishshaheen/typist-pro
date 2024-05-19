bfb08621074617b14b7ce804ea49d8d408ce63e3
nazishshaheen <nazishrapp@gmail.com>
2024-05-19 23:13:28 +0530

    added input container component

---
+++ src/components/InputContainer.js
@@ -0,0 +1,22 @@
+import '../style.css';
+import Input from './Input';
+
+function InputContainer(){
+    return(
+        <div className='Input-container'>
+            <Input/>
+            <Input/>
+            <Input/>
+            <Input/>
+            <Input/>
+            <Input/>
+            <Input/>
+            <Input/>
+            <Input/>
+            <Input/>
+            <Input/>
+        </div>
+    )
+}
+
+export default InputContainer; 
