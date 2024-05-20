62983f2f0ca4bdf9005a0f3f65c3d08e2f19d5e4
nazish shaheen <156086159+nazishshaheen@users.noreply.github.com>
2024-05-20 15:20:57 +0000

    added some functionallity to get input element

--- src/components/InputContainer.js
+++ src/components/InputContainer.js
@@ -1,20 +1,39 @@
+// import { } from '../functions/charCounter';
 import '../style.css';
 import Input from './Input';
+import React, { useEffect, useRef } from "react";
 
-function InputContainer(){
-    return(
+
+function InputContainer() {
+    useEffect(() => {
+        document.querySelector('.Input-elm').focus();
+        let allInputs = document.querySelectorAll('.Input-elm');
+        for (let input of allInputs) {
+            input.addEventListener("keyup", (e) => {
+                
+                if (e.target.value.length > 88) {
+                    e.target.nextElementSibling.focus();
+                    console.dir(e.target);
+                    // console.log(e.target.value.length);
+
+                }
+                // console.log(e.target.value.length);
+            })
+        }
+    }, []);
+    return (
         <div className='Input-container'>
-            <Input/>
-            <Input/>
-            <Input/>
-            <Input/>
-            <Input/>
-            <Input/>
-            <Input/>
-            <Input/>
-            <Input/>
-            <Input/>
-            <Input/>
+            <Input />
+            <Input />
+            <Input />
+            <Input />
+            <Input />
+            <Input />
+            <Input />
+            <Input />
+            <Input />
+            <Input />
+            <Input />
         </div>
     )
 }
