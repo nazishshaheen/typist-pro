027b0c0719c960f404fe6b2bea8b27a8fb5b4067
nazish shaheen <156086159+nazishshaheen@users.noreply.github.com>
2024-05-20 15:21:20 +0000

    added thead and tbody in table

--- src/components/Result.js
+++ src/components/Result.js
@@ -3,64 +3,69 @@ import '../style.css';
 function Result() {
     return (
         <div className='Result-container'>
-            <div class="table-responsive-sm">
+            <div className="table-responsive-sm">
                 <table className="Table">
-                    <tr>
-                        <th>A</th>
-                        <th>B</th>
-                        <th>C</th>
-                        <th>D</th>
-                        <th>E</th>
-                        <th>F</th>
-                        <th>G</th>
-                        <th>H</th>
-                        <th>I</th>
-                        <th>J</th>
-                        <th>K</th>
-                        <th>L</th>
-                        <th>M</th>
-                        <th>N</th>
-                        <th>O</th>
-                        <th>P</th>
-                        <th>Q</th>
-                        <th>R</th>
-                        <th>S</th>
-                        <th>T</th>
-                        <th>U</th>
-                        <th>V</th>
-                        <th>W</th>
-                        <th>X</th>
-                        <th>Y</th>
-                        <th>Z</th>
-                    </tr>
-                    <tr>
-                        <td id="A">0</td>
-                        <td id="B">0</td>
-                        <td id="C">0</td>
-                        <td id="D">0</td>
-                        <td id="E">0</td>
-                        <td id="F">0</td>
-                        <td id="G">0</td>
-                        <td id="H">0</td>
-                        <td id="I">0</td>
-                        <td id="J">0</td>
-                        <td id="K">0</td>
-                        <td id="L">0</td>
-                        <td id="M">0</td>
-                        <td id="N">0</td>
-                        <td id="O">0</td>
-                        <td id="P">0</td>
-                        <td id="Q">0</td>
-                        <td id="R">0</td>
-                        <td id="S">0</td>
-                        <td id="T">0</td>
-                        <td id="U">0</td>
-                        <td id="V">0</td>
-                        <td id="W">0</td>
-                        <td id="X">0</td>
-                        <td id="Y">0</td>
-                        <td id="Z">0</td>
-                    </tr>
+                    <thead>
+                        <tr>
+                            <th>A</th>
+                            <th>B</th>
+                            <th>C</th>
+                            <th>D</th>
+                            <th>E</th>
+                            <th>F</th>
+                            <th>G</th>
+                            <th>H</th>
+                            <th>I</th>
+                            <th>J</th>
+                            <th>K</th>
+                            <th>L</th>
+                            <th>M</th>
+                            <th>N</th>
+                            <th>O</th>
+                            <th>P</th>
+                            <th>Q</th>
+                            <th>R</th>
+                            <th>S</th>
+                            <th>T</th>
+                            <th>U</th>
+                            <th>V</th>
+                            <th>W</th>
+                            <th>X</th>
+                            <th>Y</th>
+                            <th>Z</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td id="A">0</td>
+                            <td id="B">0</td>
+                            <td id="C">0</td>
+                            <td id="D">0</td>
+                            <td id="E">0</td>
+                            <td id="F">0</td>
+                            <td id="G">0</td>
+                            <td id="H">0</td>
+                            <td id="I">0</td>
+                            <td id="J">0</td>
+                            <td id="K">0</td>
+                            <td id="L">0</td>
+                            <td id="M">0</td>
+                            <td id="N">0</td>
+                            <td id="O">0</td>
+                            <td id="P">0</td>
+                            <td id="Q">0</td>
+                            <td id="R">0</td>
+                            <td id="S">0</td>
+                            <td id="T">0</td>
+                            <td id="U">0</td>
+                            <td id="V">0</td>
+                            <td id="W">0</td>
+                            <td id="X">0</td>
+                            <td id="Y">0</td>
+                            <td id="Z">0</td>
+                        </tr>
+                    </tbody>
+
                 </table>
             </div>
         </div>
